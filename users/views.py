62935cda19069c370b98edac26d0bdcecc7c94c6
gradient <elizararsen@gmail.com>
2025-04-11 01:41:37 +0400

    login required decorator - redirect to target page

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -36,7 +40,6 @@ def registration(request):
             user = form.instance
             auth.login(request, user)
             messages.success(request, f"{user.username}, Вы успешно зарегистрированы и вошли в аккаунт")
-            return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
     
